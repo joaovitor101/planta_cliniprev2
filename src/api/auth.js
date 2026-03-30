444ead3e1ed60d1736b292135f99e3dbc82db715
João Kusaka | Plano Angelus <joao.kusaka@planoangelus.com.br>
2026-03-30 15:35:05 -0300

    fix: teste apenas 3

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import { api } from "./client";
 
 export async function login({ password }) {
-  const { data } = await api.post("api/auth/login", { password });
+  const { data } = await api.post("/auth/login", { password });
   return data; // { token, user }
 }
 
